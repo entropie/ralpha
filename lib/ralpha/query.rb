e0943053ea5bdc96ca2ebe78908707e958e2d2e4
Michael Trommer <mictro@gmail.com>
2011-01-23 04:12:05 +0100

    Images and States added

--- lib/ralpha/query.rb
+++ lib/ralpha/query.rb
@@ -56,11 +56,36 @@ module Ralpha
     end
   end
 
+  class Image < Struct.new(:src, :alt, :title, :width, :height)
+    def initialize(img)
+      img.each do |i|
+        attr, value = i
+        self.send("#{attr}=".to_sym, value)
+      end
+    end
+
+    def to_html
+      "<img src='#{src}' alt='#{alt}' width='#{width}' height='#{height}' title='#{title}'/>"
+    end
+  end
+
+  class States < Array
+  end
+
+  class State
+    attr_reader :name, :input
+    def initialize(name, input)
+      @name, @input = name, input
+    end
+  end
+
   class SubPod
-    attr_reader :title, :text, :pod
+    attr_reader :title, :text, :pod, :xml
+
     def initialize(title, xml, ppod)
       @pod = ppod
       @title = title
+      @xml = xml
       @text  = xml.xpath("plaintext").text
     end
 
@@ -68,10 +93,21 @@ module Ralpha
       "<Subpod @parent='#{pod.title}' @title='#{title}' '#{text}'>"
     end
 
+    def image
+      if img = xml.xpath("img").first
+        @image ||= Image.new(img)
+      end
+    end
+
+    def has_image?
+      not image.nil?
+    end
+    alias :"img?" :"has_image?"
+
     def to_s
       "#{title}: #{text}"
     end
-    
+
   end
   
   class Pod
@@ -91,7 +127,7 @@ module Ralpha
     def initialize(title, xml, query)
       @title = title
       @query = query
-
+      @xml = xml
       xml.xpath("subpod").each do |spod|
         subpods << SubPod.new(spod["title"], spod, self)
       end
@@ -101,6 +137,18 @@ module Ralpha
       "<Pod Query='#{query.query}' Title='#{title}' Subpods=#{size}>"
     end
 
+    def states
+      @states = States.new
+      xml.xpath("states").children.grep(Nokogiri::XML::Element).map{|state|
+        @states << State.new(state["name"], state["input"])
+      }
+      @states
+    end
+
+    def has_states?
+      not states.empty?
+    end
+    
     def each(&blk)
       subpods.each(&blk)
     end
@@ -109,6 +157,18 @@ module Ralpha
       subpods.size
     end
     alias :length :size
+
+    def [](int)
+      subpods[int]
+    end
+
+    def last
+      subpods.last
+    end
+
+    def first
+      subpods.first
+    end
 
   end
   
@@ -136,18 +196,27 @@ module Ralpha
       pods
     end
     
+    def to_s
+      str = "Query '#{query}' Success=#{success?}"
+      pods.each do |pod|
+        str << "\n > #{pod.title}"
+        if pod.has_states?
+          display_states = pod.states.map do |state|
+            state.name
+          end
+          str << " [#{display_states.join(", ")}]"
+        end
+        pod.each do |spod|
+          str << "\n  #{spod.to_s}"
+          if spod.has_image?
+            str << " {Image:true}"
+          end
+        end
+      end
+      str
+    end
   end
 
-  def to_s
-    str = "Query '#{query}' Success=#{success?}"
-    pods.each do |pod|
-      str << "\n > #{pod.title}"
-      pod.each do |spod|
-        str << "\n  #{spod.to_s}"
-      end
-    end
-    str << ">"
-  end
   
 end
 
